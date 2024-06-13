7ba08bded73317c1e37927489ade6f6a0e2bf23a
Niteshns <nitesh17ns@gmail.com>
2024-06-13 21:52:22 +0200

    instrumentation function -Nitesh

--- test/surfarray_tags.py
+++ test/surfarray_tags.py
@@ -1,3 +1,10 @@
+branch_coverage = {     
+    "except_import_numpy": False,       
+    "import_numpy_succes": False,         
+    "except_import_pygame.pixelcopy": False,      
+    "imports_failed": False                 
+}
+
 __tags__ = ["array"]
 
 exclude = False
@@ -5,12 +12,22 @@ exclude = False
 try:
     import numpy
 except ImportError:
+    branch_coverage["except_import_numpy"] = True
     exclude = True
 else:
+    branch_coverage["import_numpy_succes"] = True
     try:
         import pygame.pixelcopy
     except ImportError:
+        branch_coverage["except_import_pygame.pixelcopy"] = True
         exclude = True
 
 if exclude:
+    branch_coverage["imports_failed"] = True
     __tags__.extend(("ignore", "subprocess_ignore"))
+
+def print_coverage():
+    for branch, hit in branch_coverage.items():
+        print(f"{branch} was {'hit' if hit else 'not hit'}")
+
+print_coverage()
